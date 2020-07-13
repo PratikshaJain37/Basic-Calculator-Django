1c5bec6c6e01a619ed3719a5e3adbaf0c977d94d
PratikshaJain37 <pratikshajain123@gmail.com>
2020-07-13 14:58:04 +0530

    Updated for clarity

--- calculator/helpers.py
+++ calculator/helpers.py
@@ -11,7 +11,8 @@ def Factorial(number):
 
 def Calculate_Result(num1, operator, num2):
     num1 = float(num1)
-    try:
+    
+    if num2:
         num2 = float(num2)
         # Two number operations
         if operator == '+':
@@ -29,16 +30,17 @@ def Calculate_Result(num1, operator, num2):
         elif operator == '//':
             result = 'Q:  '+ str(num1//num2) + "  R:  "+ str(num1%num2) 
         else:
-            result = 'Invalid operation'
-    except ValueError:
-        result = "Please Enter a Valid Number"
-    else:
-        result = "Whoops"
+            result = 'Invalid operation with 2 numbers'
 
-    if operator == '!':
-        result = Factorial(num1)
     else:
-        pass
+        if operator == '!':
+            if num1==int(num1):
+                result = Factorial(int(num1))
+            else:
+                result = 'Invalid operation: Whole Numbers Only'
+        else:
+            result = 'Invalid operation with only 1 number'
+
 
     return result
 
