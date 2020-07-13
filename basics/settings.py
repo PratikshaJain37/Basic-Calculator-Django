a2abfb0179136384c9abe8c0cc22988d5cd364f6
PratikshaJain37 <pratikshajain123@gmail.com>
2020-07-13 14:57:16 +0530

    merged

--- basics/settings.py
+++ basics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '17+zv6h=gywc8z)z6!rqq)oq)ie$v$1)iul)atf%j9%0zb1#iz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pratiksha.pythonanywhere.com']
+ALLOWED_HOSTS = ['pratiksha.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
